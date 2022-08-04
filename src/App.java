483c6698d02b832edc680da4d5164995d53a6eef
Mario Golub <masterdiamond93@gmail.com>
2022-08-03 22:16:33 -0400

    Updating Progress

--- src/App.java
+++ src/App.java
@@ -5,17 +5,13 @@ public class App {
     
         // 2D array = an array of arrays
 
-        String[][] cars = new String[3][3];
+        String[][] cars = {
+            {"Camaro","Corvette","Silverado"},
+            {"Mustang","Ranger","F-150"},
+            {"Ferrari","Lambo","Tesla"}
+            };
 
-        cars[0][0]= "Camaro";
-        cars[0][1] = "Corvette";
-        cars[0][2] = "Silverado";
-        cars[1][0]= "Mustang";
-        cars[1][1] = "Ranger";
-        cars[1][2] = "F-150";
-        cars[2][0]= "Ferrari";
-        cars[2][1] = "Lambo";
-        cars[2][2] = "Tesla";
+
 
         for(int i = 0; i<cars.length; i++){
             System.out.println();
@@ -23,7 +19,6 @@ public class App {
                 System.out.print(cars[i][j]+" ");
             }
         }
-
 
     }
 }
