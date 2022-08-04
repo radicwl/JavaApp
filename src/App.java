d6edd32d5f0294d50c9f3035368e35830b3efed2
Mario Golub <masterdiamond93@gmail.com>
2022-08-04 17:51:13 -0400

    Java Methods

--- src/App.java
+++ src/App.java
@@ -1,30 +1,21 @@
-import java.util.*;
 
 public class App {
 
     public static void main(String[] args) {
         
-        ArrayList<ArrayList<String>> groceryList = new ArrayList();
+        // method = a block of code that is executed whenever it is called upon
 
-        ArrayList<String> bakeryList = new ArrayList();
-        bakeryList.add("pasta");
-        bakeryList.add("garlic bread");
-        bakeryList.add("donuts");
+        int x = 3;
+        int y = 4;
 
-        ArrayList<String> produceList = new ArrayList();
-        produceList.add("tomatoes");
-        produceList.add("zucchini");
-        produceList.add("peppers");
 
-        ArrayList<String> drinksList = new ArrayList();
-        drinksList.add("soda");
-        drinksList.add("coffee");
+        System.out.println(add(x,y));
+    }
 
-        groceryList.add(bakeryList);
-        groceryList.add(produceList);
-        groceryList.add(drinksList);
+    static int add(int x, int y) {
 
-        System.out.println(groceryList.get(2).get(0));
+        return x + y;
 
     }
+
 }
