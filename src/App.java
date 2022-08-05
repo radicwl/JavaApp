132cd8241e822af353fbceb7236136cefd59f64b
Mario Golub <masterdiamond93@gmail.com>
2022-08-05 13:16:07 -0400

    Printf example

--- src/App.java
+++ src/App.java
@@ -1,55 +1,37 @@
-import java.util.*;
 
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("The point of this game is to guess the number that I'm thinking of.");
-        System.out.println("You have 5 guesses to get my number, which is in between 0 and 9.");
-        Random a = new Random();
-        Scanner b = new Scanner(System.in);
-        Set<Integer> d = new HashSet<>();
-        int x = a.nextInt(9);
-        for(int i=4; i>=0; i--) {
-            int c = b.nextInt();
-            if(c==x) {
-                System.out.println("Congratulations, you got it!");
-                break;
-            }
-            else if(c>9 || c<0) {
-            System.out.println("Invalid number. Please guess between 0 and 9.");  
-            i++;
-            }
-            else {
-                if(i>1) {
-                    if(d.contains(c)) {
-                        System.out.println("You have already guessed that number. Try again.");
-                        i++;
-                    }
-                    else {
-                    System.out.println("That was incorrect.");
-                    System.out.println("You have "+i+ " guesses remaining.");
-                    d.add(c);
-                    }
-                }
-                else if(i==1) {
-                    if(d.contains(c)) {
-                        System.out.println("You have already guessed that number. Try again.");
-                        i++;
-                    }
-                    else {
-                    System.out.println("That was incorrect.");
-                    System.out.println("You have "+i+ " guess remaining.");
-                    d.add(c);
-                    }
-                }
-                else {
-                System.out.println("You did not get the number, better luck next time!");
-                break;
-                }
-            }
+       
+        boolean myBoolean = true;
+        char myChar = '@';
+        String myString = "Bro";
+        int myInt = 50;
+        double myDouble = 1000;
+
+        // [conversion-character]
+        //System.out.printf("%b", myBoolean);
+        //System.out.printf("%c", myChar);
+        //System.out.printf("%s", myString);
+        //System.out.printf("%d", myInt);
+        //System.out.printf("%f", myDouble);
+
+        //[width]
+        // minimum number of characters to be written as output
+        //System.out.printf("Hello %10s",myString);
+
+        //[precision]
+        // sets number of digits of precision when outputting floating-point values
+        //System.out.printf("You have this much money %.1f ",myDouble);
+
+        // [flags]
+        // adds an effect to output based on the flag added to format specifier
+        // - : left-justify
+        // + : output a plus ( + ) or minus ( - ) sign for a numeric value
+        // 0 : numeric values are zero-padded
+        // , : comma grouping separator if numbers > 1000
         
-        }
-        b.close();
-    }   
+        System.out.printf("You have this much money %,f ",myDouble);
+      }   
 
 }
