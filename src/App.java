0c849bc7e9c10efaea78bc6e6ac3e81dcf328a34
Mario Golub <masterdiamond93@gmail.com>
2022-08-04 21:33:38 -0400

    Number guessing game

--- src/App.java
+++ src/App.java
@@ -1,39 +1,55 @@
+import java.util.*;
 
 public class App {
 
     public static void main(String[] args) {
+        System.out.println("The point of this game is to guess the number that I'm thinking of.");
+        System.out.println("You have 5 guesses to get my number, which is in between 0 and 9.");
+        Random a = new Random();
+        Scanner b = new Scanner(System.in);
+        Set<Integer> d = new HashSet<>();
+        int x = a.nextInt(9);
+        for(int i=4; i>=0; i--) {
+            int c = b.nextInt();
+            if(c==x) {
+                System.out.println("Congratulations, you got it!");
+                break;
+            }
+            else if(c>9 || c<0) {
+            System.out.println("Invalid number. Please guess between 0 and 9.");  
+            i++;
+            }
+            else {
+                if(i>1) {
+                    if(d.contains(c)) {
+                        System.out.println("You have already guessed that number. Try again.");
+                        i++;
+                    }
+                    else {
+                    System.out.println("That was incorrect.");
+                    System.out.println("You have "+i+ " guesses remaining.");
+                    d.add(c);
+                    }
+                }
+                else if(i==1) {
+                    if(d.contains(c)) {
+                        System.out.println("You have already guessed that number. Try again.");
+                        i++;
+                    }
+                    else {
+                    System.out.println("That was incorrect.");
+                    System.out.println("You have "+i+ " guess remaining.");
+                    d.add(c);
+                    }
+                }
+                else {
+                System.out.println("You did not get the number, better luck next time!");
+                break;
+                }
+            }
         
-        // overloaded methods = methods that share the same name but have different parameters
-        // method name + parameters = method signature
-
-        double x = add(1.0,2.0,3.0,4.0);
-
-        System.out.println(x);
-    }
-
-    static int add(int a, int b) {
-        System.out.println("This is overloaded method #1");
-        return a + b;
-    }
-    static int add(int a, int b, int c) {
-        System.out.println("This is overloaded method #2");
-        return a + b + c;
-    }
-    static int add(int a, int b, int c, int d) {
-        System.out.println("This is overloaded method #3");
-        return a + b + c + d;
-    }
-    static double add(double a, double b) {
-        System.out.println("This is overloaded method #4");
-        return a + b;
-    }
-    static double add(double a, double b, double c) {
-        System.out.println("This is overloaded method #5");
-        return a + b + c;
-    }
-    static double add(double a, double b, double c, double d) {
-        System.out.println("This is overloaded method #6");
-        return a + b + c + d;
-    }
+        }
+        b.close();
+    }   
 
 }
